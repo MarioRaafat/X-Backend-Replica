4c3e251e74984d5301b7a4d2024778924ed7fc54
Alyaa Ali <eissaalyaa@gmail.com>
2025-09-09 21:56:35 +0300

    feat(auth): generate jwt in sign up

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,23 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/user/entities/user.entity';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: { expiresIn: configService.get('JWT_EXPIRATION_TIME') },
+      }),
+      inject: [ConfigService],
+    }),
+    PassportModule,
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -8,17 +8,20 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/user/entities/user.entity';
 import { Repository, EntityManager } from 'typeorm';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(User) private readonly userRepository: Repository<User>,
     private readonly entityManager: EntityManager,
+    private readonly jwtService: JwtService,
   ) {}
 
   async register(registerDto: RegisterDto) {
     const { confirmPassword, password, ...registerUser } = registerDto;
 
+    // Check if email is in use
     const existingUser = await this.userRepository.findOne({
       where: { email: registerDto.email },
     });
@@ -27,15 +30,21 @@ export class AuthService {
       throw new ConflictException('Email already exists');
     }
 
+    // Check that passwords match
     if (password !== confirmPassword) {
       throw new BadRequestException(
         'Confirmation password must match password',
       );
     }
 
+    // Hash password
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const user = new User({ ...registerUser, password: hashedPassword });
-    return await this.entityManager.save(user);
+    const createdUser = await this.entityManager.save(user);
+
+    // Create token
+    const payload = { id: createdUser.id, email: createdUser.email };
+    return this.jwtService.sign(payload);
   }
 }
