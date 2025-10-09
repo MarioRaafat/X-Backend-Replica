a8f6d526488f482ca775e6324f00f845b83946f4
shady-2004 <shadyshiko2004@gmail.com>
2025-10-09 16:56:05 +0300

    test(auth-controller):signup  unit tests

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -29,13 +29,15 @@ describe('AuthController', () => {
   afterEach(() => jest.clearAllMocks());
 
   describe('signup', () => {
+
+    const dto: RegisterDto = {
+       email: 'john@example.com',
+       password: 'StrongPass123',
+       firstName: 'John',
+       lastName: 'Doe',
+     } as RegisterDto;
+
     it('should call authService.register with the correct DTO and return result', async () => {
-      const dto: RegisterDto = {
-        email: 'john@example.com',
-        password: 'StrongPass123',
-        firstName: 'John',
-        lastName: 'Doe',
-      } as RegisterDto;
 
       const mockResult = {
         message: 'User successfully registered. Check email for verification',
@@ -49,6 +51,18 @@ describe('AuthController', () => {
       expect(mockAuthService.register).toHaveBeenCalledWith(dto);
       expect(result).toEqual(mockResult);
     });
+    
+    it('should throw if authService.register throws', async () => {
+    const dto = { email: 'error@example.com', password: '123456' } as any;
+    mockAuthService.register.mockRejectedValueOnce(new Error('Service failed'));
+
+    await expect(controller.signup(dto)).rejects.toThrow('Service failed');
+    expect(mockAuthService.register).toHaveBeenCalledTimes(1);
+    expect(mockAuthService.register).toHaveBeenCalledWith(dto);
+    });
+
   });
-  
+
+
+
 });
