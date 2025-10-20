f74e5a899cc9a3b4aabc2bef085bb464b666d07e
AmiraKhalid04 <amirakhaled928@gmail.com>
2025-10-20 23:48:31 +0300

    fix(chat): add missing TypeOrmModule.forFeature imports

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ChatController } from './chat.controller';
 import { ChatService } from './chat.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Chat, Message } from './entities';
 
 @Module({
-  controllers: [ChatController],
-  providers: [ChatService]
+    imports: [TypeOrmModule.forFeature([Chat, Message])],
+
+    controllers: [ChatController],
+    providers: [ChatService],
 })
 export class ChatModule {}
