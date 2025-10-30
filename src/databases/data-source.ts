7d6f300a4855063a17ced43865ec2d41d0d5ff4e
AmiraKhalid04 <amirakhaled928@gmail.com>
2025-10-30 14:52:32 +0300

    feat(db-seed): load env file into data source

--- src/databases/data-source.ts
+++ src/databases/data-source.ts
@@ -1,21 +1,26 @@
 import { DataSource } from 'typeorm';
+import { config } from 'dotenv';
+import { resolve } from 'path';
 import { ConfigService } from '@nestjs/config';
 import { User } from '../user/entities/user.entity';
 import { Verification } from '../verification/entities/verification.entity';
+import { Tweet } from 'src/tweets/entities/tweet.entity';
+import { TweetLike, TweetQuote, TweetReply, TweetRepost } from 'src/tweets/entities';
+config({ path: resolve(__dirname, '../../config/.env') });
+const config_service = new ConfigService();
 
-const configService = new ConfigService();
-
-export default new DataSource({
+export const AppDataSource = new DataSource({
     type: 'postgres',
-    host: process.env.POSTGRES_HOST || configService.get<string>('POSTGRES_HOST'),
-    username: process.env.POSTGRES_USERNAME || configService.get<string>('POSTGRES_USERNAME'),
-    password: process.env.POSTGRES_PASSWORD || configService.get<string>('POSTGRES_PASSWORD'),
-    database: process.env.POSTGRES_DB || configService.get<string>('POSTGRES_DB'),
+    host: process.env.POSTGRES_HOST || config_service.get<string>('POSTGRES_HOST'),
+    username: process.env.POSTGRES_USERNAME || config_service.get<string>('POSTGRES_USERNAME'),
+    password: process.env.POSTGRES_PASSWORD || config_service.get<string>('POSTGRES_PASSWORD'),
+    database: process.env.POSTGRES_DB || config_service.get<string>('POSTGRES_DB'),
     port:
         parseInt(process.env.POSTGRES_PORT || '5432') ||
-        configService.get<number>('POSTGRES_PORT') ||
+        config_service.get<number>('POSTGRES_PORT') ||
         5432,
-    entities: [User, Verification],
+    entities: [User, Verification, Tweet, TweetLike, TweetQuote, TweetRepost, TweetReply],
     migrations: ['src/migrations/*{.ts,.js}'],
     synchronize: false,
 });
+export default AppDataSource;
