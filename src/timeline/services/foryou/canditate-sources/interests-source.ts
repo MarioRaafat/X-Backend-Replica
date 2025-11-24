1ac152aa9e6d35d4fbba5a721864b05f38c59df4
AmiraKhalid04 <amirakhaled928@gmail.com>
2025-11-24 19:46:24 +0200

    fix(timeline): add random feed for users with no interests

--- src/timeline/services/foryou/canditate-sources/interests-source.ts
+++ src/timeline/services/foryou/canditate-sources/interests-source.ts
@@ -96,10 +96,77 @@ export class InterestsCandidateSource {
         );
 
         let interset_tweets = await query.getRawMany();
+
+        if (interset_tweets.length === 0) {
+            query = this.user_posts_view_repository
+                .createQueryBuilder('tweet')
+                .select([
+                    'tweet.tweet_id AS tweet_id',
+                    'tweet.profile_user_id AS profile_user_id',
+                    'tweet.tweet_author_id AS tweet_author_id',
+                    'tweet.repost_id AS repost_id',
+                    'tweet.post_type AS post_type',
+                    'tweet.type AS type',
+                    'tweet.content AS content',
+                    'tweet.type AS type',
+                    'tweet.post_date AS post_date',
+                    'tweet.images AS images',
+                    'tweet.videos AS videos',
+                    'tweet.num_likes AS num_likes',
+                    'tweet.num_reposts AS num_reposts',
+                    'tweet.num_views AS num_views',
+                    'tweet.num_quotes AS num_quotes',
+                    'tweet.num_replies AS num_replies',
+                    'tweet.created_at AS created_at',
+                    'tweet.updated_at AS updated_at',
+                    `json_build_object(
+                        'id', tweet.tweet_author_id,
+                        'username', tweet.username,
+                        'name', tweet.name,
+                        'avatar_url', tweet.avatar_url,
+                        'cover_url', tweet.cover_url,
+                        'verified', tweet.verified,
+                        'bio', tweet.bio,
+                        'followers', tweet.followers,
+                        'following', tweet.following
+                    ) AS user
+`,
+                ])
+                .where(
+                    'tweet.tweet_author_id NOT IN (SELECT muted_id FROM user_mutes WHERE muter_id = :user_id)',
+                    { user_id }
+                )
+                .andWhere(
+                    'tweet.tweet_author_id NOT IN (SELECT blocked_id FROM user_blocks WHERE blocker_id = :user_id)',
+                    { user_id }
+                )
+
+                // .orderBy('tweet.post_date', 'DESC')
+                .orderBy('RANDOM()')
+                .limit(limit)
+                .setParameter('user_id', user_id);
+
+            query = this.tweet_repository.attachUserInteractionBooleanFlags(
+                query,
+                user_id,
+                'tweet.tweet_author_id',
+                'tweet.tweet_id'
+            );
+            query = this.tweet_repository.attachRepostInfo(query);
+            query = this.paginate_service.applyCursorPagination(
+                query,
+                cursor,
+                'tweet',
+                'post_date',
+                'tweet_id'
+            );
+
+            interset_tweets = await query.getRawMany();
+        }
         interset_tweets = this.tweet_repository.attachUserFollowFlags(interset_tweets);
 
         // apply pagination
-        console.log(interset_tweets);
+        console.log(interset_tweets.length);
         const tweet_dtos = interset_tweets.map((row) => {
             const dto = plainToInstance(ScoredCandidateDTO, row, {
                 excludeExtraneousValues: true,
