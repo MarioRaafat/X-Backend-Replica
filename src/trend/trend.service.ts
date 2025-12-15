cc2dcf643ac54193986184ea03c1b3ab68791a1a
Amira Khalid <149877108+AmiraKhalid04@users.noreply.github.com>
2025-12-15 12:51:28 +0200

    Feat/fake trends v2 (#203)
    * feat(trend): increase TTL time for trend just for testing scenarios
    
    * test(trend): fix unit tests

--- src/trend/trend.service.ts
+++ src/trend/trend.service.ts
@@ -143,7 +143,7 @@ export class TrendService {
 
         //Expire after 2 hours
         // We may delegate it to trend worker
-        await this.redis_service.expire('candidates:active', 6 * 60 * 60);
+        await this.redis_service.expire('candidates:active', 24 * 60 * 60);
     }
     async insertCandidateCategories(hashtags: HashtagJobDto) {
         const pipeline = this.redis_service.pipeline();
@@ -157,7 +157,7 @@ export class TrendService {
                 if (percent >= this.CATEGORY_THRESHOLD) {
                     // Store hashtag with its category percentage as score
                     pipeline.zadd(`candidates:${category_name}`, percent, hashtag);
-                    pipeline.expire(`candidates:${category_name}`, 6 * 60 * 60);
+                    pipeline.expire(`candidates:${category_name}`, 24 * 60 * 60);
                 }
             }
         }
@@ -178,7 +178,7 @@ export class TrendService {
 
             await this.redis_service.zincrby(`hashtag:${hashtag}`, 1, time_bucket.toString());
 
-            await this.redis_service.expire(`hashtag:${hashtag}`, 6 * 60 * 60);
+            await this.redis_service.expire(`hashtag:${hashtag}`, 24 * 60 * 60);
         }
 
         await pipeline.exec();
